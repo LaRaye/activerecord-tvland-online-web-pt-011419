c12d6dff7009f6cb70f18b0b9fc05c039c4be170
LaRaye Johnson <lnj2105@gmail.com>
2019-04-11 16:13:47 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -7,6 +7,6 @@ class Actor < ActiveRecord::Base
   end 
   
   def list_roles
-    self.characters.each {|char| "#{char} - #{char.show.name}"}
+    self.characters.each {|char| "#{char.name} - #{char.show.name}"}
   end 
 end
