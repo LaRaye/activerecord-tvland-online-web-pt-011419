35a87cbeeab00274a6bcfea9717e6addcc8aabbf
LaRaye Johnson <lnj2105@gmail.com>
2019-04-11 15:40:12 +0000

    Automatically backed up by Learn

--- db/migrate/008_add_catchphrase_to_characters.rb
+++ db/migrate/008_add_catchphrase_to_characters.rb
@@ -1,3 +1,3 @@
-class AddCatchphraseToCharacters < ActiveRecord::Mirgation[5.2]
+class AddCatchphraseToCharacters < ActiveRecord::Migration[5.2]
   add_column :characters, :catchphrase, :string
 end 
